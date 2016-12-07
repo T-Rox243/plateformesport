7451a77e4e0a09b9af21fe19e3c40b70d765ea9a
unknown <lhalter@PCASYIDF106.AUSYNET.FR>
2016-12-07 17:08:32 +0100

    mise en place meca event

--- src/AppBundle/Controller/EventController.php
+++ src/AppBundle/Controller/EventController.php
@@ -40,8 +40,28 @@ class EventController extends Controller
      */
     public function addEventAction()
     {
-        
-        
+        $em = $this->getDoctrine()->getManager();
+
+        $event1 = new Evenement();
+        $event2 = new Evenement();
+        $event3 = new Evenement();
+        $event4 = new Evenement();
+
+        $event1->setName("Aikido - Stage FEDE");
+        $event2->setName("Viet Vo Dao - Competion");
+        $event3->setName("Pencak-Silat - Stage découverte");
+        $event4->setName("Histoire des arts martiaux");
+
+        $event1->setDescription("Description de l'evenement à venir, je n'ai pas specialement d'idées. Du coup ca sera le même pour tout les evenements");
+        $event2->setDescription("Description de l'evenement à venir, je n'ai pas specialement d'idées. Du coup ca sera le même pour tout les evenements");
+        $event3->setDescription("Description de l'evenement à venir, je n'ai pas specialement d'idées. Du coup ca sera le même pour tout les evenements");
+        $event4->setDescription("Description de l'evenement à venir, je n'ai pas specialement d'idées. Du coup ca sera le même pour tout les evenements");
+
+        $event1->setStartDate(date("d-m-Y", mktime(0,0,0,7,1,2017)));
+        $event2->setStartDate(date("d-m-Y", mktime(0,0,0,7,1,2017)));
+        $event3->setStartDate(date("d-m-Y", mktime(0,0,0,7,1,2017)));
+        $event4->setStartDate(date("d-m-Y", mktime(0,0,0,7,1,2017)));
+
         return $this->render('event/add_event.html.twig', array(
         ));
     }
