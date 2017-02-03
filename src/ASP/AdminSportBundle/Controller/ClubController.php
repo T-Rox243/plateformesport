3cd4bdf381d787b6ea8fb0b06fd417c99e1e4d6b
JackNightmare <louis.halter@voila.fr>
2017-02-03 01:39:04 +0100

    list Club

--- src/ASP/AdminSportBundle/Controller/ClubController.php
+++ src/ASP/AdminSportBundle/Controller/ClubController.php
@@ -4,6 +4,10 @@ namespace ASP\AdminSportBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpFoundation\Request;
+
+use AppBundle\Entity\Club;
 
 class ClubController extends Controller
 {
@@ -12,7 +16,13 @@ class ClubController extends Controller
      */
     public function adminClubAction()
     {
-        return $this->render('admin/admin_club.html.twig', array());
+    	$em = $this->getDoctrine()->getManager();
+        
+        $allClub = $em->getRepository('AppBundle:Club')->findAll();
+
+        return $this->render('admin/admin_club.html.twig', array(
+        	"allClub" => $allClub
+        ));
     }
 
 }
